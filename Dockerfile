7364f01c70a588ea3997eec11e6a7af740fc8f0a
Luke Fries <lfries@cozero.com.au>
2017-06-22 13:25:56 +1000

    gunicorn, api etc

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM python:2.7
-MAINTAINER Shekhar Gulati "shekhargulati84@gmail.com"
+MAINTAINER lfries@cozero.com.au
+
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-ENTRYPOINT ["python"]
-CMD ["app.py"]
+
+EXPOSE 8080
+
+CMD ["gunicorn", "-w", "1", "-b", ":8080", "--log-level", "info", "-t", "60", "app:app", "--worker-class", "gevent"]
--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+"""
+Basic app to test terraformed infrastructure on AWS.
+- ALB (80, 443) -> dynamic port
+- ECS dynamic port -> 8080 (container port)
+- Flask API (gunicorn)
+
+"""
 from flask import Flask
+from flask_restful import Api
+from flask import Response
+import json
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Flask inside Docker!!"
-
+@app.route('/')
+def api_root():
+    """
+    """
+    return Response(json.dumps('hello world'), status=200, mimetype='application/json')
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True)
